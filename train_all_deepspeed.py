04ce2e915e2d8ff46578f112caaf4186d4319a89
yynil <yueyu.lin@me.com>
2024-03-07 02:10:08 +0800

    change the dataloader worker to 1

--- train_all_deepspeed.py
+++ train_all_deepspeed.py
@@ -383,7 +383,7 @@ if __name__ == "__main__":
     parser.add_argument("--global-batch-size", type=int, default=8)
     parser.add_argument("--global-seed", type=int, default=0)
     parser.add_argument("--vae", type=str, choices=["ema", "mse"], default="ema")  # Choice doesn't affect training
-    parser.add_argument("--num-workers", type=int, default=1)
+    parser.add_argument("--num-workers", type=int, default=24)
     parser.add_argument("--log-every", type=int, default=100)
     parser.add_argument("--ckpt-every", type=int, default=50_000)
     parser.add_argument("--is-zip", action="store_true",default=True)
